33d32aab68f9238c8881b5ea55cda725ee57f84f
tianyawy <tianya_wy@hotmail.com>
2019-08-06 15:30:53 +0800

    init

---
+++ src/main/java/tech/victoryw/okhttpunexpectedendstreamfix/OkhttpUnexpectedEndStreamFixApplication.java
@@ -0,0 +1,13 @@
+package tech.victoryw.okhttpunexpectedendstreamfix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OkhttpUnexpectedEndStreamFixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OkhttpUnexpectedEndStreamFixApplication.class, args);
+	}
+
+}
