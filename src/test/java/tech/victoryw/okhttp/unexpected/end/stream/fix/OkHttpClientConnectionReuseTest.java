b6c1eae5403eb346b255983543863f6deeafff48
tianyawy <tianya_wy@hotmail.com>
2019-08-07 10:38:29 +0800

    use log to show the exception message

--- src/test/java/tech/victoryw/okhttp/unexpected/end/stream/fix/OkHttpClientConnectionReuseTest.java
+++ src/test/java/tech/victoryw/okhttp/unexpected/end/stream/fix/OkHttpClientConnectionReuseTest.java
@@ -1,11 +1,7 @@
 package tech.victoryw.okhttp.unexpected.end.stream.fix;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import okhttp3.Interceptor;
-import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
-import okhttp3.RequestBody;
 import okhttp3.Response;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -13,16 +9,12 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.http.HttpStatus;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import tech.victoryw.okhttp.unexpected.end.stream.fix.controller.User;
 
 import java.io.IOException;
-import java.net.SocketException;
 import java.util.logging.Logger;
 
-import static java.util.concurrent.TimeUnit.NANOSECONDS;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
@@ -54,14 +46,15 @@ class OkHttpClientConnectionReuseTest {
         }
 
         Thread.sleep(60500);
-        assertThrows(IOException.class, () -> {
-        try( Response response = okHttpClient.newCall(new Request.Builder()
-                .url(String.format("http://localhost:%d/users/self", port))
-                .get()
-                .build()).execute();) {
-            logger.info(()-> "this is no execpected throw");
-        }
+        final IOException ioException = assertThrows(IOException.class, () -> {
+            try (Response response = okHttpClient.newCall(new Request.Builder()
+                    .url(String.format("http://localhost:%d/users/self", port))
+                    .get()
+                    .build()).execute();) {
+                logger.info(() -> "this is no execpected throw");
+            }
         });
+        logger.info(String.format("The io exception is %s", ioException.getMessage()));
     }
 
     @Test
